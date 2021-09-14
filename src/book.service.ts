85b2023c11d5d5524d3bf36e563beff064c14b78
alexiscla95 <alexis-claesen@outlook.com>
2021-09-14 15:45:37 +0200

    creation of the new class for book and book service

--- src/book.service.ts
+++ src/book.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Book } from './Book'
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  private bookStore: Book[] = []
+
+  addBook(book: Book): void {
+    if (!this.bookStore.find(e => e === book)) {
+      this.bookStore.push(book)
+    }
+  }
+
+  getBook(name: string): Book | undefined {
+    return this.bookStore.find(e => e.title === name)
+  }
+
+  getBooksOf(author: string): Book[] {
+    return this.bookStore.filter(e => e.author === author)
+  }
+
+  getAllBooks(): Book[] {
+    return this.bookStore.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
+  }
+
+  getTotalNumberOfBooks(): number {
+    return this.bookStore.length
+  }
+
+  getBooksPublishedBefore(aDate: string | Date): Book[] {
+    const compareDate = new Date(aDate)
+    return this.bookStore.filter(e => e.date <= compareDate)
+  }
+}
